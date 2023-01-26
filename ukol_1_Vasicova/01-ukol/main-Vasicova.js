1b1fa7f7e87edc878d2b71e1519b0230f2737493
Eva Vašicová <miev@seznam.cz>
2023-01-26 18:34:40 +0100

    Update main-Vasicova.js

--- ukol_1_Vasicova/01-ukol/main-Vasicova.js
+++ ukol_1_Vasicova/01-ukol/main-Vasicova.js
@@ -1,15 +1,12 @@
-let title; 
-title = document.querySelector("#Title_H1");
+let title = document.querySelector("#Title_H1");
 title.style.color = "magenta";
 
-let paragraph;
-paragraph = document.querySelector("#paragraph1");
+let paragraph = document.querySelector("#paragraph1");
 paragraph.style.fontWeight = "bold";
 paragraph.style.fontStyle = "italic";
 paragraph.style.fontSize = "150%";
 
-let picture;
-picture = document.querySelector("#groundhog");
+let picture = document.querySelector("#groundhog");
 picture.src="sysel.jpg";
 
 function move_square() {
